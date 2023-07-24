6bac6d3449d4e4b31ff5f20e90418263a736387c
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-24 13:13:49 -0400

    template urls and static files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  // template: `<p>Angular</p>`,
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  // styles: [""]
 })
+
 export class AppComponent {
-  title = 'ng-basics';
+
 }
